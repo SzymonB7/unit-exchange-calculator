dafe29a68ea04f834c27cc887bd4c320000f4301
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-09-21 12:26:20 +0200

    project setup

---
+++ src/main/java/pl/babiarz/unitexchangecalculator/UnitExchangeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.babiarz.unitexchangecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitExchangeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitExchangeCalculatorApplication.class, args);
+	}
+
+}
